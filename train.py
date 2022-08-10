1560ddcc8fdc2abed1e93b04e878f150469a00b1
antitheos <antitheos@gmail.com>
2022-08-09 20:29:13 -0700

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from pytorch_lightning import Trainer
 from data.text_image_dm import TextImageDataModule
 from models import CLIPWrapper
+import torch
 
 
 def main(hparams):
